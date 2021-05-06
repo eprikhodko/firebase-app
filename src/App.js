4d9d191379c19e7fa08f3f2b59cd7bf39d0847ae
eprikhodko <eprikhodkodev@gmail.com>
2021-05-06 19:00:14 +0300

    Add firebase Storage file upload function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import {firebase} from "./lib/firebase"
 import './App.css'
 
+
 function App() {
+
+  const handleFileUpload = (event) => {
+    // get file object from the file input
+    const file = event.target.files[0]
+    firebase.storage().ref()
+    .child(file.name)
+    .put(file)
+    .then(() => {
+      console.log("file was uploaded to Firestorage")
+    })
+  }
   
   return (
     <div className="App">
-      hello
+      hello {" "}
+
+      <input 
+        type="file" 
+        onChange={handleFileUpload}
+      />
     </div>
   );
 }
