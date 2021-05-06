f8d51047c4a120c76b9becb49ac6bb66647ef95a
eprikhodko <eprikhodkodev@gmail.com>
2021-05-06 19:40:49 +0300

    Add comments to the handleFileUpload function

--- src/App.js
+++ src/App.js
@@ -7,22 +7,47 @@ function App() {
   const handleFileUpload = (event) => {
     // get file object from the file input
     const file = event.target.files[0]
-    firebase.storage().ref()
-    .child(file.name)
-    .put(file)
+    // get storage reference from firebase
+    const firebaseStorageReference = firebase.storage().ref()
+    // create file reference
+    const fileReference = firebaseStorageReference.child(file.name)
+    // upload our file from our form input to the firebase Storage
+    fileReference.put(file)
     .then(() => {
       console.log("file was uploaded to Firestorage")
     })
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
   }
   
   return (
     <div className="App">
       hello {" "}
 
-      <input 
-        type="file" 
-        onChange={handleFileUpload}
-      />
+      <form onSubmit={handleSubmit}>
+        {/* file upload */}
+        <input 
+          type="file" 
+          onChange={handleFileUpload}
+        />
+
+        {/* username input */}
+        <input
+          type="text"
+          name="username"
+          placeholder="type in your username"
+        />
+
+        <button 
+          type="button"
+          >
+            Submit
+          </button>
+      </form>
+      
+
     </div>
   );
 }
