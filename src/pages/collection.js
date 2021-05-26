08dd595ac888492ccb2fb6774fcbd078206398f4
eprikhodko <eprikhodkodev@gmail.com>
2021-05-26 12:39:09 +0300

    Map album titles from user collection to the page

--- src/pages/collection.js
+++ src/pages/collection.js
@@ -21,93 +21,51 @@ const Collection = () => {
 
     // now lets make an AJAX call to firestore and save response to the state
 
-    const [userData, setUserData] = useState([])
-    const [albumsCollection, setAlbumsCollection] = useState([])
-
     const [albums, setAlbums] = useState([])
 
     useEffect(() => {
 
-        const fetchUserAlbums = async() => {
+        const fetchAlbums = async() => {
             const userAlbums = await db.collection("albums")
             const query = userAlbums.where("usersCollection", "array-contains", "JDGEJL57awNvQNXQkxk3ClbqdMG2").get()
 
-            setAlbums(query)
+            // setAlbums(query)
             
         }
 
-        db.collection("albums").where("usersCollection", "array-contains", currentUser.uid)
-            .get()
-            .then((querySnapshot) => {
-                querySnapshot.forEach((doc) => {
-                    // doc.data() is never undefined for query doc snapshots
-                    console.log(doc.id, " => ", doc.data());
-                });
-            })
-            .catch((error) => {
-                console.log("Error getting documents: ", error);
-            });
+        const fetchUserAlbums = async() => {
+            const albumsRef = db.collection("albums")
+            const snapshot = await albumsRef.where("usersCollection", "array-contains", currentUser.uid).get()
 
-        
-            console.log(currentUser.uid)
-       
-
-        
-
-
-        const fetchUserData = async() => {
-            const user = await db.collection("users").doc(currentUser.uid).get()
-
-            // // update user document
-            // const oldUser = await db.collection("users").doc(currentUser.uid)
-            // oldUser.update({
-            //     albumsInUserCollection: ["album1", "album2"]
-            // })
-
-            // const oldUser = await db.collection("users").doc(currentUser.uid)
-            // oldUser.update({
-            //     albumsInUserCollection: ["AuO9i3l3cfrT1xQfts3k","Do8M2gFdtPrgHZekZ3iz"]
-            // })
+            // console.log(snapshot.docs[0].data())
             
-            // console.log(user.data())
-            setUserData(user.data())
-            // setAlbumData(album.data())
-            // console.log(albumsCollection)
-            // console.log(albumsCollection.docs)
-            // console.log(albumsCollection.docs[0].id)
-        }
-    
-        fetchUserData()
+        //    console.log(snapshot.docs)
 
-        // fetch all albums that exists in firebase
-        const fetchAlbums = async() => {
-            const albumsCollection = await db.collection("albums").get()
-            // for each album document in "albums" collection in fireStore, return album document and add new property of albumId which value equals to document.id
-            setAlbumsCollection(albumsCollection.docs.map(doc => {
-                return {...doc.data(), albumId: doc.id}
-            }))
-            // console.log(albumsCollection)
-            // console.log(albumsCollection.docs)
-            // console.log(albumsCollection.docs[0].id)
+            setAlbums(snapshot.docs.map(album => {
+                return album.data()
+            }))    
+
+            
+            
+           
         }
-    
-        fetchAlbums()
+
+        fetchUserAlbums()
+
        
     },[])
 
-    // console.log(userData)
-    // console.log(userData.albumsInUserCollection)
-
-    // console.log(albumsCollection)
-
-    // now we need to find albums we have in the userData.albumsInUserCollection array in the albumsCollection array.
-
+    console.log(albums)
 
     return(
         <div>
             <Header />
             <div>this is collection of {currentUser.displayName}</div>
-
+            {albums.map(album => {
+                return <div key={album.albumTitle}>
+                            {album.albumTitle}
+                        </div>
+            })}
         </div>
     )
 }
