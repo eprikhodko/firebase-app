a31737d7884c4f2ee6d7f9f4cde9b55e04f2ff45
eprikhodko <eprikhodkodev@gmail.com>
2021-07-17 20:28:43 +0300

    Use HashRouter instead of Router in App.js in order to deploy your app to the github pages and save routing functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import {HashRouter as Router, Route, Switch} from "react-router-dom"
 import * as ROUTES from "./constants/routes"
 import "./styles/App.css"
 
