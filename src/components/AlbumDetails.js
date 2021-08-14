bd2ab0e689acf043680e76962891dc3827ba8ee5
eprikhodko <eprikhodkodev@gmail.com>
2021-08-14 17:37:39 +0300

    Change user-album relationship in firestore database

--- src/components/AlbumDetails.js
+++ src/components/AlbumDetails.js
@@ -13,16 +13,20 @@ const AlbumDetails = () => {
     // const currentUser = null
 
     const params = useParams()
-    console.log(params)
+    // console.log(params)
     // destructure out albumId from params
     const {albumId} = useParams()
-    console.log(albumId)
+    // console.log(albumId)
 
     const {firebase} = useContext(FirebaseContext)
 
     // create reference to the Firestore database
     const db = firebase.firestore()
-
+    // create reference to the user-album-rel collection
+    const albumsRef = db.collection("user-album-rel")
+    
+  
+      
     // make AJAX call to get the info about the specified album
     // I can either make a call to firebase and get an album with provided Id from firestore, or I can get all collection as an array and find specific album there
 
@@ -31,23 +35,53 @@ const AlbumDetails = () => {
 
     const [isInCollection, setIsInCollection] = useState(false)
 
+    const [albumIdInUserAlbumRelCollection, setAlbumIdInUserAlbumRelCollection] = useState(null)
+
     useEffect(() => {
         const fetchAlbumData = async() => {
             const album = await db.collection("albums").doc(albumId).get()
             setAlbumData(album.data())
 
             // proceed this step only if user is logged in. currentUser is true? then setIsIncollection()
-            currentUser && setIsInCollection(album.data().albumUsers.includes(currentUser.uid))
+            // currentUser && setIsInCollection(album.data().albumUsers.includes(currentUser.uid))
             // console.log(album.data().albumUsers.includes(currentUser.uid))
         }
     
         fetchAlbumData()
 
-    },[])
+        const fetchQuerySnapshot = async() => {
+            const snapshot = await albumsRef.where("albumId", "==", albumId).where("userId", "==", currentUser.uid)
+            .get()
+            if (snapshot.empty) {
+                console.log('No matching documents.')
+                return;
+              }  
+              
+            snapshot.docs.map(doc => {
+                console.log(doc.data().userId)
+                setAlbumIdInUserAlbumRelCollection(doc.id)
+                console.log(albumIdInUserAlbumRelCollection)
+                console.log(doc.id)
+                return currentUser && setIsInCollection(doc.data().userId)
+            })
 
+            // snapshot.forEach(doc => {
+            // console.log(doc.id, '=>', doc.data())
+            // console.log(doc.data().userId)
+            // })
+           
+
+        }
+    
+        fetchQuerySnapshot()
+
+        console.log(isInCollection)
+
+    },[])
+    
     // console.log(albumUsers) 
     // console.log(currentUser.uid)
-    console.log(`album is in collection: ${isInCollection}`)
+    // console.log(`album is in collection: ${isInCollection}`)
 
     // console.log(albumUsers.includes(currentUser.uid))
 
@@ -57,18 +91,42 @@ const AlbumDetails = () => {
     // console.log(isAlbumInUserCollection)
 
     const handleRemoveFromCollection = async() => {
+
+        const res = await db.collection("user-album-rel").doc(albumIdInUserAlbumRelCollection).delete()
+        setIsInCollection(false)
+        // const albumRef = db.collection("user-album-rel").doc(albumIdInUserAlbumRelCollection)
+        // const doc = await albumRef.get();
+        // if (!doc.exists) {
+        // console.log('No such document!');
+        // } else {
+        // console.log('Document data:', doc.data());
+        // }
+    }
+
+    const RemoveFromCollection = async() => {
         const album = await db.collection("albums").doc(albumId)
         album.update({albumUsers: firebase.firestore.FieldValue.arrayRemove(currentUser.uid)})
         setIsInCollection(false)
     }
 
     const handleAddToCollection = async() => {
+        const res = await db.collection("user-album-rel").add({
+            albumId: albumId,
+            userId: currentUser.uid
+        })
+        console.log('Added document with ID: ', res.id);
+        setAlbumIdInUserAlbumRelCollection(res.id)
+        setIsInCollection(true)
+    }
+
+    const AddToCollection = async() => {
         const album = await db.collection("albums").doc(albumId)
         album.update({albumUsers: firebase.firestore.FieldValue.arrayUnion(currentUser.uid)})
         setIsInCollection(true)
     }
 
     const albumButtons = 
+    // check if album data is loaded. If it is loaded, then proceed to show a paragraph or buttons to add or remove album from collection
     // check if user is logged in
     !currentUser ? <p className="paragraph-notification">Login to add this album to your collection</p>
     // if user is logged in, check if album is in user collection
@@ -113,6 +171,7 @@ const AlbumDetails = () => {
                     {/* YOU CAN ALSO JUST ADD LOADING SKELETON, so you wont see the gap when data is still fetching and add to collection and remove from collection buttons wont flashing */
                     // if current user is logged in, then check if album is in his collection. If user is not logged in, then show him a login link
                     }
+                    
                     {albumButtons}
                 </div>
             </div>
