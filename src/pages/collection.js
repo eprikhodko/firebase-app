c876f62c756d0853a699024ba5ce4978876ccdf0
eprikhodko <eprikhodkodev@gmail.com>
2021-08-19 14:34:42 +0300

    Show albums in user collection sorted by date added by default

--- src/pages/collection.js
+++ src/pages/collection.js
@@ -23,11 +23,13 @@ const Collection = () => {
     const [albumsData, setAlbumsData] = useState([])
     const [albumsIds, setAlbumsIds] = useState([])
 
+    const [sortedByDateAddedToUserCollection, setSortedByDateAddedToUserCollection] = useState([])
+
     useEffect(() => {
         const fetchAlbumsInUserCollection = async() => {
             // lets fetch albums in user collection
             const snapshot = await db.collection("users").doc(currentUser.uid).collection("albumsInUserCollection")
-            .orderBy("dateAdded", "desc")
+            .orderBy("dateAdded", "asc")
             .get()
 
             // const albumsInUserCollection = snapshot.docs.map(doc => {
@@ -42,26 +44,50 @@ const Collection = () => {
             })
 
             setAlbumsIds(albumsIdsArray)
-            console.log(albumsIdsArray)
+            console.log("this initial ids array from user collection", albumsIdsArray)
 
             // firestore has query limit for 'in' query, so we need to split our albums ids in chunks of 10 items, and then make a query
             const chunk = 10
             const albumsDataArray = []
             for (let i = 0; i < albumsIdsArray.length; i+= chunk) {
                 let temporaryArray = albumsIdsArray.slice(i, i + chunk)
-                console.log(temporaryArray)
+                // console.log("this is temporary array", temporaryArray)
 
                 const albumsDataSnapshot = await albumsDataRef.where(firebase.firestore.FieldPath.documentId(), 'in', temporaryArray)
                 .get()
-                console.log(albumsDataSnapshot)
+                // console.log("this is albums data snapshot", albumsDataSnapshot)
+                // albumsDataSnapshot.forEach(doc => {
+                //     console.log(doc.data())
+                // })
 
                 const mappedAlbums = albumsDataSnapshot.docs.map(doc => {
                     return doc.data()
                 })
+                // console.log("this is mapped albums", mappedAlbums)
 
                 albumsDataArray.push(mappedAlbums)
 
-                setAlbumsData(albumsDataArray.flat())
+               
+
+
+                const flattenedAlbums = albumsDataArray.flat()
+                console.log("this is flattened albums array from firestore", flattenedAlbums )
+                console.log("this is albums Ids", albumsIdsArray)
+
+                const sortedAlbums = []
+                for (let i = 0; i < albumsIdsArray.length; i++) {
+                    const found = flattenedAlbums.find(element => element.albumId === albumsIdsArray[i])
+                    // console.log("found", found)
+                    sortedAlbums.push(found)
+                }
+
+                // console.log("this is sorted albums", sortedAlbums)
+                setSortedByDateAddedToUserCollection(sortedAlbums)
+                console.log("this is sorted albums", sortedAlbums)
+                // console.log("this is last album in sorted albums array", sortedAlbums[sortedAlbums.length-1])
+
+                sortedAlbums[sortedAlbums.length-1] && setAlbumsData(sortedAlbums)
+                // setSortedByDateAddedToUserCollection(albumsDataArray.flat())
             }
         }
 
@@ -69,7 +95,14 @@ const Collection = () => {
 
     },[])
 
-    console.log(albumsData)
+    // console.log(albumsData)
+    const albumIdsFromFirestore = albumsData.map(album => {
+        return album.albumId
+    })
+
+    console.log("this is ids from firestore", albumIdsFromFirestore)
+
+
 
     const albumComponents = albumsData.map(album => {
         return(
@@ -100,12 +133,11 @@ const Collection = () => {
     })
 
     const handleSortByYear = () => {
-
         const albums = albumsData.map(album => {
             return album.year
         })
 
-        const sortedByYear = albumsData.slice().sort((a,b) => {
+        const sortedByYear = [...albumsData].sort((a,b) => {
             if (a.year < b.year) return -1
             if (a.year > b.year) return 1
             return 0
@@ -119,6 +151,14 @@ const Collection = () => {
         // console.log(testSortArray)
         
         return setAlbumsData(sortedByYear)
+    }
+
+    const handleSortByAddedToCollection = () => {
+
+        setAlbumsData(sortedByDateAddedToUserCollection)
+
+        console.log("sorted by date added to collection")
+
     }
 
     return(
@@ -136,7 +176,13 @@ const Collection = () => {
                                 onClick={handleSortByYear}
                                 > 
                                 Year
-                            </button>
+                        </button>
+                        <button 
+                                type="button"
+                                onClick={handleSortByAddedToCollection}
+                                > 
+                                Added to collection
+                        </button>
                     </div>
                     
                     <div className="container-albums">
